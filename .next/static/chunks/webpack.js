c9d8fe1ea6274d69b36039f4ff5b5246928260e6
Oleg Vasilev <vasek_tr@mail.ru>
2024-05-02 23:05:34 +0300

    02 05 commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3448d60a4812df41"; }
+/******/ 		__webpack_require__.h = function() { return "d209565ae782d3d7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,78 +1,18 @@
-/* import { Children, useState } from "react";
-import {
-  GameInfo,
-  GameTitle,
-  GameField,
-  useGameState,
-} from "../components/game"; */
 import { Header } from "../components/header";
-/* import { GameSymbol } from "../components/game/game-symbol";
-import { UiModal } from "../components/uikit/ui-modal";
-import { UiButton } from "../components/uikit/ui-button"; */
 import { Game } from "../components/game-new";
+import { UiTextField } from "../components/uikit/ui-text-field";
 
 export default function HomePage() {
-  /*  const [playersCount] = useState(4);
-  const {
-    cells,
-    currentMove,
-    nextMove,
-    handleCellClick,
-    winnerSequence,
-    handlePlayerTimeIsOver,
-    winnerSymbol,
-  } = useGameState(playersCount); */
-
   return (
     <HomePageLayout header={<Header />}>
       <Game />
-
-      {/* <GameTitle playersCount={playersCount} />
-      <GameInfo
-        className="mt-2"
-        playersCount={playersCount}
-        currentMove={currentMove}
-        isWinner={!!winnerSymbol} //!! наличие --- проще говоря не undefined
-        onPlayerTimeOver={handlePlayerTimeIsOver}
-        winnerSymbol={winnerSymbol}
+      <UiTextField
+        label="Email"
+        required
+        placeholder="placeholder"
+        helperText="Helper text"
+        /* errorText="bad" */
       />
-      {winnerSymbol && (
-        <div className="my-4">
-          <GameSymbol symbol={winnerSymbol} />
-        </div>
-      )}
-      <UiModal
-        width="md"
-        isOpen={!!winnerSymbol}
-        onClose={() => {
-          console.log("close");
-        }}
-      >
-        <UiModal.Header>Игра завершена!</UiModal.Header>
-        <UiModal.Body>
-          <div className="text-sm">
-            Победитель: <span className="text-teal-600">VasilevOl</span>
-          </div>
-        </UiModal.Body>
-        <UiModal.Footer>
-          <UiButton size="md" variant="outline">
-            Вернуться
-          </UiButton>
-          <UiButton size="md" variant="primary">
-            Играть снова
-          </UiButton>
-        </UiModal.Footer>
-      </UiModal>
-      <GameField
-        className="mt-6"
-        playersCount={playersCount}
-        cells={cells}
-        currentMove={currentMove}
-        nextMove={nextMove}
-        handleCellClick={handleCellClick}
-        winnerSequence={winnerSequence}
-        winnerSymbol={winnerSymbol}
-      /> */}
     </HomePageLayout>
   );
 }
